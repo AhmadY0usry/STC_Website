a1756d0159fea2c8119d6973fdc366799f6a3b0c
AhmadY0usry <a.yosry344@gmail.com>
2023-06-24 20:13:40 +0300

    adding parameter for choosing browser to run on

--- src/test/java/Test_In_Arabic/Test_Base.java
+++ src/test/java/Test_In_Arabic/Test_Base.java
@@ -1,9 +1,12 @@
 package Test_In_Arabic;
-
 import Pages.Page_Base;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 import org.testng.asserts.SoftAssert;
 
 public class Test_Base {
@@ -11,9 +14,16 @@ public class Test_Base {
     protected Page_Base pageBase;
     protected SoftAssert softAssert;
     @BeforeClass
-    public void setUp()
+    @Parameters("browser")
+    public void setUp(@Optional("Edge")String browserName)
     {
-        driver= new EdgeDriver();
+        if(browserName.equalsIgnoreCase("edge"))
+            driver= new EdgeDriver();
+            else if (browserName.equalsIgnoreCase("Chrome"))
+            driver= new ChromeDriver();
+            else if (browserName.equalsIgnoreCase("Firefox"))
+            driver= new FirefoxDriver();
+
         driver.get("https://subscribe.stctv.com/sa-ar");
         driver.manage().window().maximize();
         pageBase =new Page_Base(driver);
